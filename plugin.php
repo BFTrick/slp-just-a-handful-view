4ceb63a6163fa25b3ba466d03d83ef428c11d011
Patrick Rauland <bftrick@gmail.com>
2012-09-19 11:53:46 -0500

    initial commit

---
+++ plugin.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Plugin Name: Just a Handful View for Store Locator Plus
+Plugin URI: http://www.charlestonsw.com/products/store-locator-plus/
+Description: This extension takes the standard layout in Store Locator Plus and rearranges the layout to better show a short list of venues.
+Version: 1.0
+Author: Patrick Rauland
+Author URI: http://www.patrickrauland.com
+
+Copyright (C) 2012 Patrick Rauland
+
+*/
+
+class SLPJustAHandfulView
+{
+	public function __construct()
+	{
+		add_action( 'init', array( &$this, 'init' ) );
+	}
+
+    public function init()
+    {
+        //remove the search form
+    	add_action('slp_render_search_form',array('SLPEnhancedSearch','slp_render_search_form'),9);
+
+    	// Search Functionality Hook
+ 		add_filter('slp_javascript_results_string', array('SLPEnhancedResults','mangle_results_output'), 90);
+    }
+}
+
+/**
+ * Main SLP Enhanced Results Class
+ */
+Class SLPEnhancedSearch 
+{
+	/**
+	 * Method: slp_render_search_form
+	 */
+	function slp_render_search_form() 
+	{
+		//remove the search form - we don't need that jazz
+		remove_action('slp_render_search_form',array('SLPlus_UI','slp_render_search_form'));
+	}
+}
+
+/**
+ * Main SLP Enhanced Results Class
+ */
+ class SLPEnhancedResults 
+ {
+/**
+ * Method: mangle_results_output
+ */
+	function mangle_results_output($resultString) {
+
+		$locationListing = 
+		"{0} <br/>
+		{6} <br/>
+		{3} <br/>
+		{4} <br/>
+		{5}";
+		
+		return $locationListing;
+		
+	 }
+ }
+
+//instantiate the plugin
+new SLPJustAHandfulView();
+
+//That's it! Thank you very much and good night.
