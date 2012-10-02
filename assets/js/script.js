eba7c1ec9e9a32ed561668d0bcc70119cb812e43
Patrick Rauland <bftrick@gmail.com>
2012-10-02 10:06:58 -0500

    adding listener for map load. closes #4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 jQuery(document).ready(function($) {
-    
+	
 	//when a user clicks on a location in the results list add a class for CSS to style
 	$("#map_sidebar .results_entry").live("click", function(){
-		$(this).toggleClass('jahv-selected');
+		$("#map_sidebar .results_entry").removeClass('jahv-selected');
+		$(this).addClass('jahv-selected');
 	});
 
 	//open the first location by default
+	$('#map_sidebar').bind('contentchanged',function() {
+		//simulate a click on the first location to open it up
+		$("#map_sidebar .results_entry:first-child").click();
+	});
+
+	//listen to the clicks on the map
 	//TODO - waiting for a hook
 
 });
